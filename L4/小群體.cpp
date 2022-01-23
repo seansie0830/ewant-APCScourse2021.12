78b9a753f7d45d3d8949e4173db1df11ab88c6ce
seansie <seansie@gmail.com>
2022-01-23 22:36:42 +0800

    小群體final

--- L4/小群體.cpp
+++ L4/小群體.cpp
@@ -1,7 +1,5 @@
-// 內有debug 痕跡
-// 預計使用標準版上zerojudge 重測
+// tested AC(36ms, 716KB)
 #include<iostream>
-//#include<cstdlib>
 using namespace std;
 int main(){
     int n,i,no=1,first,track;
@@ -9,26 +7,17 @@ int main(){
     int belongs[n]={0},friends[n];
     for(i=0;i<n;i++) cin>>friends[i];
     for(i=0;i<n;i++){
-        //cout<<"²Ä"<<i+1<<"¦¸·j´M"<<endl;
-        //system("pause");
         first=i;
         if(belongs[i]==0){
                 track=friends[i];
             while(true){
-                //cout<<"track"<<track<<endl;
-                //cout<<"first"<<first<<endl;
-                //system("pause");
                 if(track==first) {
                     belongs[i]=no;
                     no++;
-                    //cout<<"no="<<no<<endl;
                     break;
             }
                 else {
-                        //cout<<"continue"<<track<<endl;
                         belongs[track]=no;
-                        //system("pause");
-
                 }
                 track=friends[track];
             }
