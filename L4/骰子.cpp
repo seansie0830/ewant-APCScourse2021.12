63b0b1fe82ae2a38ee000cba2cb73ecb1f0a0175
seansie <seansie@gmail.com>
2022-01-24 13:07:54 +0800

    change to vscode

---
+++ L4/骰子.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main (){
+    int n,i;
+    cin>>n;
+    int front[n],right[n],top[n],a,b;
+    for(i=0;i<n;i++){
+        cin>>a>>b;
+        if(a>0&&b>0){
+            // swap
+        }
+        else if(b==-1){
+            //向前旋轉
+        }
+        else if(b==-2){
+            //向右旋轉
+        }
+    }
+    cout<<"Hello"<<endl;
+    system("pause");
+}
