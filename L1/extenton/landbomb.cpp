09348dbbfc5f6ed96e0e65ed3b627ae3235fbc73
seansie <seansie@gmail.com>
2022-03-22 20:06:06 +0800

    更新踩地雷與升級  #1commit

--- L1/extenton/landbomb.cpp
+++ L1/extenton/landbomb.cpp
@@ -1,2 +1,24 @@
 // 新版本(2022.3.22)  升級成完整的踩地雷
 //
+#include<iostream>
+using namespace std;
+int width,height,bomb_num;
+void show(int*);
+void mark(int*);
+int main(){
+    // initialization
+    cout<<"請輸入地圖的長"<<endl;
+    cin>>width;
+    cout<<"請輸入地圖的高"<<endl;
+    cin>>height;
+    cout<<"請輸入地雷的數量"<<endl;
+    cin>>bomb_num;
+    int map[height][width]={0};
+    cin>>width;
+}
+void show(int *map_ref){
+
+}
+void mark(int *map_ref) {
+
+}
