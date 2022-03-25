3cf33c4e94670a582eb7ca20ea5a2bf4954e9d3f
seansie <seansie@gmail.com>
2022-03-25 21:09:17 +0800

    border check ,init func

--- L1/extenton/landbomb.cpp
+++ L1/extenton/landbomb.cpp
@@ -1,18 +1,14 @@
 // 新版本(2022.3.22)  升級成完整的踩地雷
 // ! the matrix isn't initialized yet
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int width,height,bomb_num,i,j;
+void init(void);
 void show(int*,int,int);
 void mark(int*);
+void bury_bomb(int*,int,int,int);
 int main(){
-    // initialization
-    cout<<"請輸入地圖的長"<<endl;
-    cin>>width;
-    cout<<"請輸入地圖的高"<<endl;
-    cin>>height;
-    cout<<"請輸入地雷的數量"<<endl;
-    cin>>bomb_num;
+    init();
     int map[height][width];
     for(int i=0;i<height;i++)for(int j=0;j<width;j++)map[i][j]=0;
     show(&map[0][0],width,height);
@@ -27,6 +23,43 @@ void show(int *map_ref,int width,int height){
         cout<<endl<<endl;
     }
 }
+void init(){
+    srand(time(NULL));
+    width_ask: cout<<"請輸入地圖的長"<<endl;
+    cin>>width;
+    if(width<1) {
+        cout<<"請重新輸入比零大的數字"<<endl;
+        goto width_ask;
+    }
+    height_ask: cout<<"請輸入地圖的高"<<endl;
+    cin>>height;
+    if(height<1) {
+        cout<<"請重新輸入比零大的數字"<<endl;
+        goto height_ask;
+    }
+    cout<<"請輸入地雷的數量"<<endl;
+    bomb_ask: cin>>bomb_num;
+    if(bomb_num>width*height){ 
+        cout<<"請重新輸入比所有地雷座標(寬*高)數量小的數字"<<endl;
+        goto bomb_ask;
+    }
+    else if(bomb_num<1){
+        cout<<"請重新輸入比零大的數字"<<endl;
+        goto bomb_ask;
+    }
+}
 void mark(int *map_ref) {
 
+}
+void bury_bomb(int *map_ref,int bomb_num,int width,int height){
+    int pick_id;
+    while(bomb_num>0){
+        pick_id =rand()%width*height+1;
+        if(*(map_ref+pick_id)==0){
+            // bury_bomb at the specified place
+            *(map_ref+pick_id)=1;
+            bomb_num--;
+        }
+        else continue;
+    }
 }
